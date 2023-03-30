a25995839ff2ab297619fef205119d2370e85c3d
0xpaprhnds <yokeyeong@me.com>
2023-03-31 00:44:41 +0200

    Update ChatBubble.tsx

--- src/components/Chat/ChatBubble.tsx
+++ src/components/Chat/ChatBubble.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const ChatBubble = ({ role, message }: Props) => {
   return role === "assistant" || role === "system" ? (
-    <div className="flex items-center gap-2 my-2">
+    <div className="flex items-center gap-2 mx-1 my-2 md:mx-0">
       <AiOutlineRobot className="p-2.5 rounded-full w-12 h-12 bg-base-300" />
       <p
         className={`${
@@ -21,7 +21,7 @@ const ChatBubble = ({ role, message }: Props) => {
       {/* <Timer start={10} /> */}
     </div>
   ) : (
-    <div className="flex items-center justify-end gap-2 my-2">
+    <div className="flex items-center justify-end gap-2 mx-1 my-2 md:mx-0">
       <p className="max-w-[280px] md:max-w-2xl px-6 py-3 text-black whitespace-pre-line bg-gray-300 shadow-2xl rounded-2xl">
         {message}
       </p>
